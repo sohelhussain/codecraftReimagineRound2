ae9e44099d91edaa3331154464876a01d097471c
Sneha Sharnagat <snehasharnagat234@gmail.com>
2024-05-14 12:47:51 +0530

    correct the js code

--- script.js
+++ script.js
@@ -55,18 +55,16 @@ gsap.from("#main #content .text h1", {
   },
 })
 
-const buttons = document.querySelectorAll("button");
+document.addEventListener('DOMContentLoaded', (event) => {
+  const button = document.querySelector(".button-three");
 
-buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    const currentState = button.getAttribute("data-state");
+      const currentState = button.getAttribute("aria-expanded");
 
-    if (!currentState || currentState === "closed") {
-      button.setAttribute("data-state", "opened");
-      button.setAttribute("aria-expanded", "true");
-    } else {
-      button.setAttribute("data-state", "closed");
-      button.setAttribute("aria-expanded", "false");
-    }
+      if (currentState === "true") {
+          button.setAttribute("aria-expanded", "false");
+      } else {
+          button.setAttribute("aria-expanded", "true");
+      }
   });
 });
