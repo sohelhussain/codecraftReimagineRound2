3e14417b28098cda8de18fce35485b37202bff30
sohelhussain <sohelhussaing@gmail.com>
2024-05-14 12:07:06 +0530

    clean code

--- script.js
+++ script.js
@@ -46,27 +46,3 @@ Shery.mouseFollower({
   duration: 1,
 });
 
-// code by sneha
-gsap.from("#main #content .text h1", {
-  y: 200,
-  opacity: 0,
-  stagger: {
-    amount: 1
-  },
-})
-
-const buttons = document.querySelectorAll("button");
-
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const currentState = button.getAttribute("data-state");
-
-    if (!currentState || currentState === "closed") {
-      button.setAttribute("data-state", "opened");
-      button.setAttribute("aria-expanded", "true");
-    } else {
-      button.setAttribute("data-state", "closed");
-      button.setAttribute("aria-expanded", "false");
-    }
-  });
-});
