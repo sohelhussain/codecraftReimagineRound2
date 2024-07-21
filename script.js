d8ea09e126f71f0613dca6f4f75a5d3b4eb845cb
sohelhussain <sohelhussaing@gmail.com>
2024-07-21 14:13:25 +0530

    addding code

--- script.js
+++ script.js
@@ -293,79 +293,76 @@ function clut() {
     end: `100% top`,
   });
 }
-clut();
+// clut();
 
 // code by sneha **--- page1 animation ----**
 
 const page1 = (e) => {
-  Shery.imageEffect("#img-container", {
-    style: 2, //Select Style,
-    gooey: true,
-      debug: true, // Debug Panel
-    config: {
-      resolutionXY: { value: 100 },
-      distortion: { value: true },
-      mode: { value: -3 },
-      mousemove: { value: 0 },
-      modeA: { value: 1 },
-      modeN: { value: 0 },
-      speed: { value: 1, range: [-500, 500], rangep: [-10, 10] },
-      frequency: { value: 50, range: [-800, 800], rangep: [-50, 50] },
-      angle: { value: 0.5, range: [0, 3.141592653589793] },
-      waveFactor: { value: 1.4, range: [-3, 3] },
-      color: { value: 10212607 },
-      pixelStrength: { value: 3, range: [-20, 100], rangep: [-20, 20] },
-      quality: { value: 5, range: [0, 10] },
-      contrast: { value: 1, range: [-25, 25] },
-      brightness: { value: 1, range: [-1, 25] },
-      colorExposer: { value: 0.18, range: [-5, 5] },
-      strength: { value: 0.2, range: [-40, 40], rangep: [-5, 5] },
-      exposer: { value: 8, range: [-100, 100] },
-      zindex: { value: 999 },
-      aspect: { value: 1.5472401939960634 },
-      ignoreShapeAspect: { value: true },
-      shapePosition: { value: { x: 0, y: 0 } },
-      shapeScale: { value: { x: 0.5, y: 0.5 } },
-      shapeEdgeSoftness: { value: 0, range: [0, 0.5] },
-      shapeRadius: { value: 0, range: [0, 2] },
-      currentScroll: { value: 0 },
-      scrollLerp: { value: 0.07 },
-      gooey: { value: true },
-      infiniteGooey: { value: true },
-      growSize: { value: 4, range: [1, 15] },
-      durationOut: { value: 1, range: [0.1, 5] },
-      durationIn: { value: 1.5, range: [0.1, 5] },
-      displaceAmount: { value: 0.5 },
-      masker: { value: false },
-      maskVal: { value: 1, range: [1, 5] },
-      scrollType: { value: 0 },
-      geoVertex: { range: [1, 64], value: 1 },
-      noEffectGooey: { value: true },
-      onMouse: { value: 0 },
-      noise_speed: { value: 0.2, range: [0, 10] },
-      metaball: { value: 0.2, range: [0, 2], _gsap: { id: 8 } },
-      discard_threshold: { value: 0.5, range: [0, 1] },
-      antialias_threshold: { value: 0, range: [0, 0.1] },
-      noise_height: { value: 0.5, range: [0, 2] },
-      noise_scale: { value: 10, range: [0, 100] },
-    },
-    //   preset: "./presets/wigglewobble.json",
-  });
+  function sheryEffect() {
+    Shery.imageEffect("#img-container", {
+      style: 2, //Select Style,
+      gooey: true,
+      // debug: true,
+      config: {
+        resolutionXY: { value: 100 },
+        distortion: { value: true },
+        mode: { value: -10 },
+        mousemove: { value: 0 },
+        modeA: { value: 1 },
+        modeN: { value: 0 },
+        speed: { value: 1, range: [-500, 500], rangep: [-10, 10] },
+        frequency: { value: 50, range: [-800, 800], rangep: [-50, 50] },
+        angle: { value: 0.5, range: [0, 3.141592653589793] },
+        waveFactor: { value: 1.4, range: [-3, 3] },
+        color: { value: 10212607 },
+        pixelStrength: { value: 3, range: [-20, 100], rangep: [-20, 20] },
+        quality: { value: 5, range: [0, 10] },
+        contrast: { value: 1, range: [-25, 25] },
+        brightness: { value: 1, range: [-1, 25] },
+        colorExposer: { value: 0.18, range: [-5, 5] },
+        strength: { value: 0.2, range: [-40, 40], rangep: [-5, 5] },
+        exposer: { value: 8, range: [-100, 100] },
+        zindex: { value: 999 },
+        aspect: { value: 1.5472401939960634 },
+        ignoreShapeAspect: { value: true },
+        shapePosition: { value: { x: 0, y: 0 } },
+        shapeScale: { value: { x: 0.5, y: 0.5 } },
+        shapeEdgeSoftness: { value: 0, range: [0, 0.5] },
+        shapeRadius: { value: 0, range: [0, 2] },
+        currentScroll: { value: 0 },
+        scrollLerp: { value: 0.07 },
+        gooey: { value: true },
+        infiniteGooey: { value: true },
+        growSize: { value: 4, range: [1, 15] },
+        durationOut: { value: 1, range: [0.1, 5] },
+        durationIn: { value: 1.5, range: [0.1, 5] },
+        displaceAmount: { value: 0.5 },
+        masker: { value: true },
+        maskVal: { value: 1.37, range: [1, 5] },
+        scrollType: { value: 0 },
+        geoVertex: { range: [1, 64], value: 1 },
+        noEffectGooey: { value: false },
+        onMouse: { value: 0 },
+        noise_speed: { value: 0.84, range: [0, 10] },
+        metaball: { value: 0.2, range: [0, 2], _gsap: { id: 8 } },
+        discard_threshold: { value: 0.5, range: [0, 1] },
+        antialias_threshold: { value: 0, range: [0, 0.1] },
+        noise_height: { value: 0.5, range: [0, 2] },
+        noise_scale: { value: 10, range: [0, 100] },
+      },
+    });
+  }
+  sheryEffect();
 
-  //  Ensure GSAP is loaded before running this script
   document.addEventListener("DOMContentLoaded", function () {
-    var h2s = document.querySelectorAll("h2.elemtags");
-    var pTags = document.querySelectorAll("p.elemtags"); // Select all p tags with class 'elemtags'
-    var imgs = document.querySelectorAll("#img-container img"); // Select all images within #img-container
+    var elem1 = document.querySelectorAll(".elem");
+    var imgs = document.querySelectorAll("#img-container img");
 
-    // Initialize index outside of the click event listener to avoid redeclaration
-    var index = 0;
-
-    // Add click event listener to each image
-    imgs.forEach(function (img) {
-      img.addEventListener("click", function () {
-        if (index < h2s.length - 1 && index < pTags.length - 1) {
-          // Animate h2 tags
+    elem1.forEach(function (elem) {
+      var h2s = elem.querySelectorAll("h2");
+      imgs.forEach(function (img) {
+        var index = 0;
+        img.addEventListener("click", function () {
           gsap.to(h2s[index], {
             top: "-=100%",
             ease: "power3.out",
@@ -374,26 +371,14 @@ const page1 = (e) => {
               gsap.set(this._targets[0], { top: "100%" });
             },
           });
-          gsap.to(pTags[index], {
-            top: "-=100%",
-            ease: "power3.out",
-            duration: 1,
-            onComplete: function () {
-              gsap.set(this._targets[0], { top: "100%" });
-            },
-          });
-          index++;
+          index === h2s.length - 1 ? (index = 0) : index++;
+
           gsap.to(h2s[index], {
             top: "-=100%",
             ease: "power3.out",
             duration: 1,
           });
-          gsap.to(pTags[index], {
-            top: "-=100%",
-            ease: "power3.out",
-            duration: 1,
-          });
-        }
+        });
       });
     });
   });
@@ -617,7 +602,7 @@ const makeParticals = (e) => {
   };
   animate();
 };
-makeParticals();
+// makeParticals();
 
 const forth = (e) => {
   var tl = gsap.timeline({
